45bc567ae11a123e786e82da7b59099fdb09602c
Jules Royet <jules.royet.pc@gmail.com>
2025-01-29 16:19:01 +0100

    ⚡️ Reglage socket

--- packages/stakeirb-front/src/socket.js
+++ packages/stakeirb-front/src/socket.js
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000')
+const socket = io('http://julesroyet-projects.me:3000')
 
 export function useSocket() {
   const isSocketConnected = ref(false)
