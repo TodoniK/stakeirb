04cf144a70f5c588997d9f8a01f760c72afdabc1
Jules Royet <jules.royet.pc@gmail.com>
2025-02-02 16:05:21 +0100

    🔥 Again socket TLS

--- packages/stakeirb-front/src/socket.js
+++ packages/stakeirb-front/src/socket.js
@@ -1,8 +1,10 @@
-// socket.js
 import { ref, onMounted } from 'vue'
 import io from 'socket.io-client'
 
-const socket = io('https://julesroyet-projects.me/api');
+const socket = io('https://julesroyet-projects.me', {
+  path: '/socket.io/', // Spécifiez le chemin pour les WebSockets
+  transports: ['websocket'], // Forcer l'utilisation de WebSocket
+})
 
 export function useSocket() {
   const isSocketConnected = ref(false)
@@ -10,12 +12,14 @@ export function useSocket() {
   onMounted(() => {
     socket.on('connect', () => {
       isSocketConnected.value = true
+      console.log('Socket connected')
     })
 
     socket.on('disconnect', () => {
       isSocketConnected.value = false
+      console.log('Socket disconnected')
     })
   })
 
-  return { socket, isSocketConnected }
+  return { isSocketConnected }
 }
