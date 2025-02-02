03e7cde6237cc781eb3df4b1589b3600ca474963
Jules Royet <jules.royet.pc@gmail.com>
2025-02-02 16:17:53 +0100

    🐛 Oui

--- packages/stakeirb-front/src/socket.js
+++ packages/stakeirb-front/src/socket.js
@@ -1,9 +1,9 @@
+// socket.js
 import { ref, onMounted } from 'vue'
 import io from 'socket.io-client'
 
 const socket = io('https://julesroyet-projects.me', {
-  path: '/socket.io/', // Spécifiez le chemin pour les WebSockets
-  transports: ['websocket'], // Forcer l'utilisation de WebSocket
+  path: '/socket.io/'
 })
 
 export function useSocket() {
@@ -12,14 +12,12 @@ export function useSocket() {
   onMounted(() => {
     socket.on('connect', () => {
       isSocketConnected.value = true
-      console.log('Socket connected')
     })
 
     socket.on('disconnect', () => {
       isSocketConnected.value = false
-      console.log('Socket disconnected')
     })
   })
 
-  return { isSocketConnected }
+  return { socket, isSocketConnected }
 }
