de4c3bc166d3640f7bba8720a923322c35fb59ed
TodoniK <jules.royet.pc@gmail.com>
2026-09-17 06:53:18 +0200

    fix: proxy backend through frontend

--- packages/stakeirb-front/src/socket.js
+++ packages/stakeirb-front/src/socket.js
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 import io from 'socket.io-client'
 
-const socket = io(import.meta.env.VITE_SOCKET_URL, {
+const socket = io(import.meta.env.VITE_SOCKET_URL || window.location.origin, {
   path: '/socket.io/'
 })
 
